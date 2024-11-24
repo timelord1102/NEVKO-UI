70e21a002efdd8367115a5c4a8f18fd5d7699e11
dotFelixan <babetr@gmail.com>
2024-11-24 18:17:36 +0300

    Fix Ukraine Localization (+Add More Lang)

--- FancyHeader/FancyHeaderTT.js
+++ FancyHeader/FancyHeaderTT.js
@@ -24,23 +24,39 @@ const translations = {
   ru: "Активности",
   en: "Activities",
   es: "Actividades",
-  br: "Atividade",
-  bg: "Дейност",
-  ge: "Aktivita",
-  ua: "Діяльність",
+  de: "Aktivitäten",
+  fr: "Activités",
+  it: "Attività",
+  pt: "Atividades",
+  nl: "Activiteiten",
+  pl: "Aktywności",
+  uk: "Діяльність",
+  zh: "活动",
+  ja: "アクティビティ",
+  ko: "활동",
+  ar: "الأنشطة",
+  hi: "गतिविधियाँ",
+  tr: "Etkinlikler",
+  sv: "Aktiviteter",
+  da: "Aktiviteter",
+  fi: "Aktiviteetit",
+  no: "Aktiviteter",
 };
 
 function getCurrentLanguage() {
   let lang = document.documentElement.lang;
+  
   if (!lang) {
     const metaLang = document.querySelector('meta[name="language"]');
     if (metaLang) {
       lang = metaLang.getAttribute('content');
     }
   }
+  
   if (!lang) {
     lang = navigator.language || navigator.userLanguage;
   }
+  
   return lang.split('-')[0];
 }
 
@@ -57,6 +73,7 @@ function setLanguage() {
 let main = document.getElementsByClassName("HoGkIKTQnkTEFGjqO-GMl");
 let main_acc = document.getElementsByClassName('_2jXHP0742MyApMUVUM8IFn')[0].cloneNode(true);
 let hr_acc = document.getElementsByClassName('_2jXHP0742MyApMUVUM8IFn')[4].cloneNode(true);
+
 setLanguage();
 
 main_acc.addEventListener('click', function(event) {
@@ -68,3 +85,8 @@ main[0].insertBefore(hr_acc, main[0].firstElementChild);
 main[0].insertBefore(main_acc, main[0].firstElementChild);
 
 document.addEventListener('languagechange', setLanguage);
+
+function changeLanguage(lang) {
+  document.documentElement.lang = lang;
+  setLanguage();
+}
